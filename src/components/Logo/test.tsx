3f0b10444fe3b008d854d16d56d3f06348e9884d
Marcos Andre <marcosdev.me@gmail.com>
2022-07-16 11:26:31 -0300

    feat: implements a logo

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,5 +1,6 @@
 import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
+import 'jest-styled-components'
 
 import Logo from '.'
 
@@ -18,5 +19,33 @@ describe('<Logo />', () => {
     expect(screen.getByLabelText(/won games/i).parentElement).toHaveStyle({
       color: '#030517'
     })
+  })
+
+  it('should render a normal logo when size is default', () => {
+    renderWithTheme(<Logo />)
+
+    expect(screen.getByLabelText(/won games/i).parentElement).toHaveStyle({
+      width: '11rem'
+    })
+  })
+
+  it('should render a bigger logo', () => {
+    renderWithTheme(<Logo size="large" />)
+
+    expect(screen.getByLabelText(/won games/i).parentElement).toHaveStyle({
+      width: '20rem'
+    })
+  })
+
+  it('should render a bigger logo without text if hideOnMobile', () => {
+    renderWithTheme(<Logo hideOnMobile />)
+
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyleRule(
+      'width',
+      '5.8rem',
+      {
+        media: '(max-width: 768px)'
+      }
+    )
   })
 })
