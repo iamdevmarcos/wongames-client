7166d677e04f4193d4dc88ba679454a55f11cb3d
Marcos Andre <marcosdev.me@gmail.com>
2022-07-16 09:41:33 -0300

    feat: create util for test

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,13 +1,10 @@
 import { render, screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Logo from '.'
 
 describe('<Logo />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Logo />)
-
-    expect(screen.getByRole('heading', { name: /Logo/i })).toBeInTheDocument()
-
-    expect(container.firstChild).toMatchSnapshot()
+  it('should render a white label by default', () => {
+    renderWithTheme(<Logo />)
   })
 })
