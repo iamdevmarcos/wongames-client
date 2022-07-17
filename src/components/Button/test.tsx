3633cb8e4e98f080d6fc00122e380035f366dac2
Marcos Andre <marcosdev.me@gmail.com>
2022-07-17 20:09:16 -0300

    feat: add button component

--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -1,17 +1,21 @@
-import { screen } from '@testing-library/react'
+import { screen, fireEvent } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import Button from '.'
 
+import { AddShoppingCart } from '@styled-icons/material-outlined/AddShoppingCart'
+
 describe('<Button />', () => {
   it('should render a medium button by default', () => {
-    renderWithTheme(<Button>Buy now</Button>)
+    const { container } = renderWithTheme(<Button>Buy now</Button>)
 
     expect(screen.getByRole('button', { name: /buy now/i })).toHaveStyle({
       height: '4rem',
       'font-size': '1.4rem',
       padding: '0.8rem 3.2rem'
     })
+
+    expect(container.firstChild).toMatchSnapshot()
   })
 
   it('should render a small button', () => {
@@ -39,5 +43,24 @@ describe('<Button />', () => {
     expect(screen.getByRole('button', { name: /buy now/i })).toHaveStyle({
       width: '100%'
     })
+  })
+
+  it('should render an icon version', () => {
+    renderWithTheme(
+      <Button>
+        <AddShoppingCart data-testid="icon" /> Buy now
+      </Button>
+    )
+
+    expect(screen.getByText(/buy now/i)).toBeInTheDocument()
+    expect(screen.getByTestId('icon')).toBeInTheDocument()
+  })
+
+  it('should call onClick when user clicks', () => {
+    const onClick = jest.fn()
+    renderWithTheme(<Button onClick={onClick}>Buy now</Button>)
+
+    fireEvent.click(screen.getByText(/buy now/i))
+    expect(onClick).toHaveBeenCalled()
   })
 })
