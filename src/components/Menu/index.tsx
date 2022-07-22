1f3d7f5365e1a83e541c52d1e8cf9997b44a9feb
Marcos Andre <marcosdev.me@gmail.com>
2022-07-22 20:15:03 -0300

    implements mobile menu (#39)
    * feat: implements mobile menu
    
    * test: implements tests for open/close menu
    
    * feat: implements design
    
    * style: add transition

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,58 @@
+import { useState } from 'react'
+
+import { Menu2 as MenuIcon } from '@styled-icons/remix-fill/Menu2'
+import { ShoppingCart as ShoppingCartIcon } from '@styled-icons/material-outlined/ShoppingCart'
+import { Search as SearchIcon } from '@styled-icons/material-outlined/Search'
+import { Close as CloseIcon } from '@styled-icons/material-outlined/Close'
+
+import Logo from 'components/Logo'
+
+import * as S from './styles'
+import Button from 'components/Button'
+
+const Menu = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <S.Wrapper>
+      <S.IconWrapper onClick={() => setIsOpen(true)}>
+        <MenuIcon aria-label="Open Menu" />
+      </S.IconWrapper>
+
+      <S.LogoWrapper>
+        <Logo hideOnMobile />
+      </S.LogoWrapper>
+
+      <S.MenuGroup>
+        <S.IconWrapper>
+          <SearchIcon aria-label="Search" />
+        </S.IconWrapper>
+
+        <S.IconWrapper>
+          <ShoppingCartIcon aria-label="Open Shopping Cart" />
+        </S.IconWrapper>
+      </S.MenuGroup>
+
+      <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
+        <CloseIcon aria-label="Close Menu" onClick={() => setIsOpen(false)} />
+
+        <S.MenuNav>
+          <S.MenuLink href="#">Home</S.MenuLink>
+          <S.MenuLink href="#">Explore</S.MenuLink>
+        </S.MenuNav>
+
+        <S.RegisterBox>
+          <Button fullWidth size="large">
+            Log in now
+          </Button>
+          <span>or</span>
+          <S.CreateAccount href="#" title="Sign Up">
+            Sign Up
+          </S.CreateAccount>
+        </S.RegisterBox>
+      </S.MenuFull>
+    </S.Wrapper>
+  )
+}
+
+export default Menu
---
+++ src/components/Menu/stories.tsx
@@ -0,0 +1,16 @@
+import { Story, Meta } from '@storybook/react'
+import Menu from '.'
+
+export default {
+  title: 'Menu',
+  component: Menu
+} as Meta
+
+export const Default: Story = () => <Menu />
+
+Default.parameters = {
+  layout: 'fullscreen',
+  backgrounds: {
+    default: 'dark'
+  }
+}
