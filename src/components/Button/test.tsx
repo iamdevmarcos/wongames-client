481f94a15e99467bd32f161394c947714293d2f1
Marcos Andre <marcosdev.me@gmail.com>
2022-09-02 06:41:20 -0300

    feat: add button

--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -63,4 +63,19 @@ describe('<Button />', () => {
     fireEvent.click(screen.getByText(/buy now/i))
     expect(onClick).toHaveBeenCalled()
   })
+
+  it('should render button as a link', () => {
+    const { debug, container } = renderWithTheme(
+      <Button as="a" href="/link">
+        Buy now
+      </Button>
+    )
+
+    debug(container)
+
+    expect(screen.getByRole('link', { name: /buy now/i })).toHaveAttribute(
+      'href',
+      '/link'
+    )
+  })
 })
