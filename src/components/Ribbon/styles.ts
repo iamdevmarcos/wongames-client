49122af1d015775cdf583b1362f431a50a419a6f
Marcos Andre <marcosdev.me@gmail.com>
2022-09-03 10:07:30 -0300

    feat: add ribbon styles

--- src/components/Ribbon/styles.ts
+++ src/components/Ribbon/styles.ts
@@ -1,17 +1,39 @@
 import styled, { css, DefaultTheme } from 'styled-components'
 import { RibbonColors, RibbonProps } from '.'
+import { darken } from 'polished'
 
 const wrapperModifiers = {
   color: (theme: DefaultTheme, color: RibbonColors) => css`
     background-color: ${theme.colors[color]};
+
+    &::before {
+      border-left-color: ${darken(0.2, theme.colors[color])};
+      border-top-color: ${darken(0.2, theme.colors[color])};
+    }
   `,
   normal: (theme: DefaultTheme) => css`
     height: 3.6rem;
+    padding: 0 ${theme.spacings.small};
     font-size: ${theme.font.sizes.small};
+    right: -2rem;
+
+    &::before {
+      top: 3.6rem;
+      border-top-width: 1rem;
+      border-right-width: 2rem;
+    }
   `,
   small: (theme: DefaultTheme) => css`
     height: 2.6rem;
     font-size: ${theme.font.sizes.xsmall};
+    padding: 0 ${theme.spacings.xsmall};
+    right: -1.5rem;
+
+    &::before {
+      top: 2.6rem;
+      border-top-width: 0.7rem;
+      border-right-width: 1.5rem;
+    }
   `
 }
 
@@ -19,7 +41,25 @@ type WrapperProps = Omit<RibbonProps, 'children'>
 
 export const Wrapper = styled.div<WrapperProps>`
   ${({ theme, color, size }) => css`
-    ${!!color && wrapperModifiers.color(theme, color)}
-    ${!!size && wrapperModifiers[size](theme)}
+    position: absolute;
+    top: ${theme.spacings.xsmall};
+    display: flex;
+    align-items: center;
+    font-weight: ${theme.font.bold};
+    color: ${theme.colors.white};
+
+    &::before {
+      content: '';
+      position: absolute;
+      right: 0;
+      border-style: solid;
+      border-left-width: 0rem;
+      border-right-color: transparent;
+      border-bottom-color: transparent;
+      border-bottom-width: 1rem;
+    }
+
+    ${!!color && wrapperModifiers.color(theme, color)};
+    ${!!size && wrapperModifiers[size](theme)};
   `}
 `
