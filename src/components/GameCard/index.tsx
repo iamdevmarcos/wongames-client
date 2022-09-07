2091a8b5815b1b8b91bb4aec5e5b2b769b835d38
Marcos Andre <marcosdev.me@gmail.com>
2022-09-07 15:19:20 -0300

    feat: add styles to gamecard

--- src/components/GameCard/index.tsx
+++ src/components/GameCard/index.tsx
@@ -16,17 +16,19 @@ const GameCard = ({ title, developer, img, price }: GameCardProps) => (
     <S.ImageBox>
       <img src={img} alt={title} />
     </S.ImageBox>
-    <S.Info>
-      <S.Title>{title}</S.Title>
-      <S.Developer>{developer}</S.Developer>
-    </S.Info>
-    <S.FavButton role="button">
-      <FavoriteBorder aria-label="Add to wishlist" />
-    </S.FavButton>
-    <S.BuyBox>
-      <S.Price>{price}</S.Price>
-      <Button icon={<AddShoppingCart />} size="small" />
-    </S.BuyBox>
+    <S.Content>
+      <S.Info>
+        <S.Title>{title}</S.Title>
+        <S.Developer>{developer}</S.Developer>
+      </S.Info>
+      <S.FavButton role="button">
+        <FavoriteBorder aria-label="Add to wishlist" />
+      </S.FavButton>
+      <S.BuyBox>
+        <S.Price>{price}</S.Price>
+        <Button icon={<AddShoppingCart />} size="small" />
+      </S.BuyBox>
+    </S.Content>
   </S.Wrapper>
 )
 
