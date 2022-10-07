590f73c95154d6cc1e13ffc2a1362373674338ae
Marcos Andre <marcosdev.me@gmail.com>
2022-10-07 19:51:11 -0300

    feat: add tests to Slider and create mock to match media

--- src/components/Slider/test.tsx
+++ src/components/Slider/test.tsx
@@ -1,13 +1,23 @@
+import 'match-media-mock'
 import { render, screen } from '@testing-library/react'
 
 import Slider from '.'
 
 describe('<Slider />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Slider />)
+  it('should render children as slider item', () => {
+    render(
+      <Slider settings={{ slidesToShow: 1, infinite: false }}>
+        <p>Item 1</p>
+        <p>Item 2</p>
+      </Slider>
+    )
 
-    expect(screen.getByRole('heading', { name: /Slider/i })).toBeInTheDocument()
+    expect(
+      screen.getByText(/item 1/i).parentElement?.parentElement
+    ).toHaveClass('slick-slide')
 
-    expect(container.firstChild).toMatchSnapshot()
+    expect(
+      screen.getByText(/item 2/i).parentElement?.parentElement
+    ).toHaveClass('slick-slide')
   })
 })
