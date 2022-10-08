f5ce501b296f7590195a6f9026c91a2a79227841
Marcos Andre <marcosdev.me@gmail.com>
2022-10-08 14:34:27 -0300

    style: adjust global styles

--- src/components/Container/test.tsx
+++ src/components/Container/test.tsx
@@ -15,23 +15,5 @@ describe('<Container />', () => {
       'max-width',
       theme.grid.container
     )
-
-    expect(container.firstChild).toMatchInlineSnapshot(`
-      .c0 {
-        max-width: 130rem;
-        margin-left: auto;
-        margin-right: auto;
-        padding-left: calc(3.2rem / 2);
-        padding-right: calc(3.2rem / 2);
-      }
-
-      <div
-        class="c0"
-      >
-        <span>
-          Won Games
-        </span>
-      </div>
-    `)
   })
 })
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -34,6 +34,11 @@ const GlobalStyles = createGlobalStyle`
     box-sizing: border-box;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+
+    &::before,
+    &::after {
+      box-sizing: inherit;
+    }
   }
 
   ${({ theme }) => css`
@@ -44,6 +49,7 @@ const GlobalStyles = createGlobalStyle`
     body {
       font-family: ${theme.font.family};
       font-size: ${theme.font.sizes.medium};
+      background-color: ${theme.colors.mainBg};
     }
   `}
 
