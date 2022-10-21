4ca92f7249adf5e3d1b0cc1185229a529256a91d
Marcos Andre <marcosdev.me@gmail.com>
2022-10-21 09:02:50 -0300

    add typecheck before commit

--- src/components/Highlight/stories.tsx
+++ src/components/Highlight/stories.tsx
@@ -1,5 +1,5 @@
 import { Story, Meta } from '@storybook/react'
-import Highlight, { HightlightProps } from '.'
+import Highlight, { HighlightProps } from '.'
 
 import item from './mock'
 
@@ -9,13 +9,13 @@ export default {
   args: { ...item }
 } as Meta
 
-export const Default: Story<HightlightProps> = (args) => (
+export const Default: Story<HighlightProps> = (args) => (
   <div style={{ maxWidth: '104rem', margin: '0 auto' }}>
     <Highlight {...args} />
   </div>
 )
 
-export const WithFloatImage: Story<HightlightProps> = (args) => (
+export const WithFloatImage: Story<HighlightProps> = (args) => (
   <div style={{ maxWidth: '104rem', margin: '0 auto' }}>
     <Highlight {...args} />
   </div>
--- src/components/Highlight/styles.ts
+++ src/components/Highlight/styles.ts
@@ -1,8 +1,8 @@
 import styled, { css } from 'styled-components'
 import media from 'styled-media-query'
-import { HightlightProps } from '.'
+import { HighlightProps } from '.'
 
-type WrapperProps = Pick<HightlightProps, 'backgroundImage' | 'alignment'>
+type WrapperProps = Pick<HighlightProps, 'backgroundImage' | 'alignment'>
 
 const wrapperModifiers = {
   right: () => css`
