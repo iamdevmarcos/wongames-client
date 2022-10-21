5de559b257a1d3eba442a1a39358275b2f710d8e
Marcos Andre <marcosdev.me@gmail.com>
2022-10-21 15:01:58 -0300

    add labelColor

--- src/components/Checkbox/styles.ts
+++ src/components/Checkbox/styles.ts
@@ -1,3 +1,12 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import { CheckboxProps } from '.'
 
 export const Wrapper = styled.main``
+
+type LabelProps = Pick<CheckboxProps, 'labelColor'>
+
+export const Label = styled.label<LabelProps>`
+  ${({ theme, labelColor }) => css`
+    color: ${theme.colors[labelColor!]};
+  `}
+`
--- src/components/Checkbox/test.tsx
+++ src/components/Checkbox/test.tsx
@@ -16,4 +16,20 @@ describe('<Checkbox />', () => {
     // get label by text
     expect(screen.getByText(/checkbox label/i)).toHaveAttribute('for', 'check')
   })
+
+  it('should render without label', () => {
+    renderWithTheme(<Checkbox />)
+
+    expect(screen.queryByLabelText('Checkbox')).not.toBeInTheDocument()
+  })
+
+  it('should render with black label', () => {
+    renderWithTheme(
+      <Checkbox label="checkbox label" labelFor="check" labelColor="black" />
+    )
+
+    expect(screen.getByText(/checkbox label/i)).toHaveStyle({
+      color: '#030517'
+    })
+  })
 })
