b2dd5fde8df81b563df812265b8140dee48086ba
Marcos Mendes <marcosdev.me@gmail.com>
2022-12-04 10:08:00 -0300

    feat: add isChecked option to checkbox

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -3,29 +3,40 @@ import * as S from './styles'
 
 export type CheckboxProps = {
   onCheck?: (status: boolean) => void
+  isChecked?: boolean
   label?: string
   labelFor?: string
   labelColor?: 'white' | 'black'
+  value?: string | ReadonlyArray<string> | number
 } & InputHTMLAttributes<HTMLInputElement>
 
 const Checkbox = ({
   onCheck,
+  isChecked = false,
   label,
   labelFor = '',
-  labelColor = 'white'
+  labelColor = 'white',
+  value,
+  ...rest
 }: CheckboxProps) => {
-  const [checked, setChecked] = useState(false)
+  const [checked, setChecked] = useState(isChecked)
 
   const onChange = () => {
     const status = !checked
     setChecked(status)
 
-    if (onCheck) onCheck(status)
+    !!onCheck && onCheck(status)
   }
 
   return (
     <S.Wrapper>
-      <S.Input id={labelFor} type="checkbox" onChange={onChange} />
+      <S.Input
+        id={labelFor}
+        type="checkbox"
+        onChange={onChange}
+        value={value}
+        {...rest}
+      />
 
       {!!label && (
         <S.Label htmlFor={labelFor} labelColor={labelColor}>
