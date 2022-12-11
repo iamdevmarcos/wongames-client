a03dcdf154fcd5d685a06663ed0c163889661cd6
Marcos Mendes <marcosdev.me@gmail.com>
2022-12-11 15:33:17 -0300

    fix: fixing svg logo bug

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -5,6 +5,12 @@ import 'jest-styled-components'
 import Logo from '.'
 
 describe('<Logo />', () => {
+  it('should render the logo with id passed', () => {
+    const { container } = renderWithTheme(<Logo id="myId" />)
+
+    expect(container.querySelector('#paint_linear_myId')).toBeInTheDocument()
+  })
+
   it('should render a white label by default', () => {
     renderWithTheme(<Logo />)
 
