b03219f7c82554adc5e0d33b38286e9c78e645e1
Marcos Mendes <marcosdev.me@gmail.com>
2022-12-29 20:26:32 -0300

    feat: add sign in and sign up

--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -1,5 +1,10 @@
+import FormSignIn from 'components/FormSignIn'
 import AuthTemplate from 'templates/Auth'
 
 export default function SignIn() {
-  return <AuthTemplate title="Sign In">children</AuthTemplate>
+  return (
+    <AuthTemplate title="Sign In">
+      <FormSignIn />
+    </AuthTemplate>
+  )
 }
--- src/pages/sign-up.tsx
+++ src/pages/sign-up.tsx
@@ -1,5 +1,10 @@
+import FormSignUp from 'components/FormSignUp'
 import AuthTemplate from 'templates/Auth'
 
 export default function SignUp() {
-  return <AuthTemplate title="Sign Up">children</AuthTemplate>
+  return (
+    <AuthTemplate title="Sign Up">
+      <FormSignUp />
+    </AuthTemplate>
+  )
 }
