27ad88c113e430e15350f018af687aa507d684e4
Marcos Mendes <marcosdev.me@gmail.com>
2023-01-01 19:44:52 -0300

    refactor: using jest mock

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,8 @@ module.exports = {
   collectCoverageFrom: [
     'src/**/*.ts(x)?',
     '!src/**/stories.tsx',
-    '!src/pages/**'
+    '!src/pages/**/*.tsx',
+    '!src/styles/**/*.ts'
   ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   modulePaths: ['<rootDir>/src/', '<rootDir>/.jest'],
--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -20,30 +20,49 @@ const props = {
   freeHighlight: highlightMock
 }
 
-describe.skip('<Home />', () => {
+jest.mock('components/Menu', () => {
+  return {
+    __esModule: true,
+    default: function Mock() {
+      return <div data-testid="mockMenu"></div>
+    }
+  }
+})
+
+jest.mock('components/Footer', () => {
+  return {
+    __esModule: true,
+    default: function Mock() {
+      return <div data-testid="mockFooter"></div>
+    }
+  }
+})
+
+jest.mock('components/Showcase', () => {
+  return {
+    __esModule: true,
+    default: function Mock() {
+      return <div data-testid="mockShowcase"></div>
+    }
+  }
+})
+
+jest.mock('components/BannerSlider', () => {
+  return {
+    __esModule: true,
+    default: function Mock() {
+      return <div data-testid="mockBannerSlider"></div>
+    }
+  }
+})
+
+describe('<Home />', () => {
   it('should render menu and footer', () => {
     renderWithTheme(<Home {...props} />)
 
-    expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
-    expect(
-      screen.getByRole('heading', { name: /follow us/i })
-    ).toBeInTheDocument()
-    expect(screen.getAllByRole('img', { name: /won games/i })).toHaveLength(2)
-
-    expect(screen.getByRole('heading', { name: /news/i })).toBeInTheDocument()
-    expect(
-      screen.getByRole('heading', { name: /most popular/i })
-    ).toBeInTheDocument()
-    expect(
-      screen.getByRole('heading', { name: /upcomming/i })
-    ).toBeInTheDocument()
-    expect(
-      screen.getByRole('heading', { name: /free games/i })
-    ).toBeInTheDocument()
-
-    // banner
-    expect(screen.getAllByText(/defy death 1/i)).toHaveLength(1)
-    // card game ( 5 sections com 4 cards cada = 5x1 = 5)
-    expect(screen.getAllByText(/population zero/i)).toHaveLength(5)
+    expect(screen.getByTestId('mockMenu')).toBeInTheDocument()
+    expect(screen.getByTestId('mockBannerSlider')).toBeInTheDocument()
+    expect(screen.getByTestId('mockFooter')).toBeInTheDocument()
+    expect(screen.getAllByTestId('mockShowcase')).toHaveLength(5)
   })
 })
