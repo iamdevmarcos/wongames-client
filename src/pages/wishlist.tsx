53ccd8b2dc21187c305100eaf3f82ede5f0b494d
Marcos Mendes <marcosdev.me@gmail.com>
2023-05-12 13:29:57 -0300

    feat: implements wishlist

---
+++ src/pages/wishlist.tsx
@@ -0,0 +1,5 @@
+import WishlistTemplate from 'templates/Wishlist'
+
+export default function Wishlist() {
+  return <WishlistTemplate />
+}
---
+++ src/templates/Wishlist/index.tsx
@@ -0,0 +1,16 @@
+import { Container } from 'components/Container'
+import React from 'react'
+import Base from 'templates/Base'
+import Heading from 'components/Heading'
+
+const Wishlist = () => (
+  <Base>
+    <Container>
+      <Heading lineLeft lineColor="secondary">
+        Wishlist
+      </Heading>
+    </Container>
+  </Base>
+)
+
+export default Wishlist
