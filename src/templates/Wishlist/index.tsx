fa9cb7ac46607b49813feb639d93c981e01a61da
Marcos Mendes <marcosdev.me@gmail.com>
2023-05-12 13:57:21 -0300

    feat: implements grid

--- src/templates/Wishlist/index.tsx
+++ src/templates/Wishlist/index.tsx
@@ -6,6 +6,7 @@ import Heading from 'components/Heading'
 import GameCard, { GameCardProps } from 'components/GameCard'
 import { HighlightProps } from 'components/Highlight'
 import Showcase from 'components/Showcase'
+import { Grid } from 'components/Grid'
 
 export type WishlistProps = {
   games?: GameCardProps[]
@@ -24,9 +25,11 @@ const Wishlist = ({
         Wishlist
       </Heading>
 
-      {games?.map((game, index) => (
-        <GameCard key={`wishlist-${index}`} {...game} />
-      ))}
+      <Grid>
+        {games?.map((game, index) => (
+          <GameCard key={`wishlist-${index}`} {...game} />
+        ))}
+      </Grid>
     </Container>
 
     <Showcase
