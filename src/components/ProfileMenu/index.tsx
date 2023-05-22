74e76b483bc770281f94bf1009abbefe9a20e188
Marcos Mendes <marcosdev.me@gmail.com>
2023-05-22 10:44:59 -0300

    feat: implements profile menu

---
+++ src/components/ProfileMenu/index.tsx
@@ -0,0 +1,47 @@
+import Link from 'next/link'
+import {
+  AccountCircle,
+  CreditCard,
+  ExitToApp,
+  FormatListBulleted
+} from 'styled-icons/material-outlined'
+
+import * as S from './styles'
+
+export type ProfileMenuProps = {
+  activeLink?: '/profile/me' | '/profile/cards' | '/profile/orders'
+}
+
+const ProfileMenu = ({ activeLink }: ProfileMenuProps) => (
+  <S.Nav>
+    <Link href="/profile/me" passHref>
+      <S.Link isActive={activeLink === '/profile/me'} title="My Profile">
+        <AccountCircle size={24} />
+        <span>My Profile</span>
+      </S.Link>
+    </Link>
+
+    <Link href="/profile/cards" passHref>
+      <S.Link isActive={activeLink === '/profile/cards'} title="My cards">
+        <CreditCard size={24} />
+        <span>My cards</span>
+      </S.Link>
+    </Link>
+
+    <Link href="/profile/orders" passHref>
+      <S.Link isActive={activeLink === '/profile/orders'} title="My orders">
+        <FormatListBulleted size={24} />
+        <span>My orders</span>
+      </S.Link>
+    </Link>
+
+    <Link href="/logout" passHref>
+      <S.Link>
+        <ExitToApp size={24} />
+        <span>Sign out</span>
+      </S.Link>
+    </Link>
+  </S.Nav>
+)
+
+export default ProfileMenu
---
+++ src/components/ProfileMenu/test.tsx
@@ -0,0 +1,30 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+import theme from 'styles/theme'
+
+import ProfileMenu from '.'
+
+describe('<ProfileMenu />', () => {
+  it('should render the menu', () => {
+    const { container } = renderWithTheme(<ProfileMenu />)
+
+    expect(
+      screen.getByRole('link', { name: /my profile/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('link', { name: /my cards/i })).toBeInTheDocument()
+    expect(screen.getByRole('link', { name: /my orders/i })).toBeInTheDocument()
+    expect(screen.getByRole('link', { name: /sign out/i })).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should render the menu with an active link by default', () => {
+    renderWithTheme(<ProfileMenu activeLink="/profile/cards" />)
+
+    expect(screen.getByRole('link', { name: /my cards/i })).toHaveStyle({
+      background: theme.colors.primary,
+      color: theme.colors.white
+    })
+  })
+})
