1201ea2428eb5baf256b8d424cfad69637ac3ff1
Marcos Mendes <marcosdev.me@gmail.com>
2023-05-26 10:46:52 -0300

    feat: implements form profile

---
+++ src/components/FormProfile/index.tsx
@@ -0,0 +1,49 @@
+import Button from 'components/Button'
+import Heading from 'components/Heading'
+import TextField from 'components/TextField'
+
+import * as S from './styles'
+
+const FormProfile = () => (
+  <S.Wrapper>
+    <Heading lineBottom color="black" size="small">
+      My profile
+    </Heading>
+
+    <S.Form>
+      <TextField
+        name="name"
+        placeholder="Name"
+        label="Name"
+        initialValue="John Doe"
+      />
+
+      <TextField
+        name="email"
+        type="email"
+        placeholder="E-mail"
+        label="E-mail"
+        initialValue="johndoe@gmail.com"
+        disabled
+      />
+
+      <TextField
+        name="password"
+        type="password"
+        placeholder="Type your password"
+        label="Password"
+      />
+
+      <TextField
+        name="new_password"
+        type="password"
+        placeholder="New Password"
+        label="New Password"
+      />
+
+      <Button size="large">Save</Button>
+    </S.Form>
+  </S.Wrapper>
+)
+
+export default FormProfile
